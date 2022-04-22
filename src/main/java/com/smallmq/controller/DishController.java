c714e90776a5204848829363722cf03a41a97d13
MQ <2271443486@qq.com>
2022-04-22 18:16:48 +0800

    分页查询菜品时候解决分类名称不显示问题

--- src/main/java/com/smallmq/controller/DishController.java
+++ src/main/java/com/smallmq/controller/DishController.java
@@ -7,13 +7,18 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.smallmq.dto.DishDto;
 import com.smallmq.pojo.Dish;
+import com.smallmq.service.CategoryService;
 import com.smallmq.service.DishFlavorService;
 import com.smallmq.service.DishService;
 import com.smallmq.utils.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -26,19 +31,40 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("page")
     public Response<Page> page(@RequestParam(value = "page", defaultValue = "1") Integer page,
                                @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
                                @RequestParam(value = "name", required = false) String name) {
-        // 分页查询
-        IPage<Dish> pageInfo = new Page<>(page, pageSize);
+        // 分页查询改进 page<Dish> 查询数据 , page<DishDto> 返回数据
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+        // 查询条件
         LambdaQueryWrapper<Dish> wrapper = new LambdaQueryWrapper<>();
         if (name != null) {
             wrapper.like(Dish::getName, name);
         }
-        Page<Dish> page1 = dishService.page((Page)pageInfo, wrapper);
-        log.info("pageInfo:{}", page1);
-        return Response.success(page1);
+        dishService.page(pageInfo, wrapper);
+        // 将数据转换为DishDto
+        BeanUtils.copyProperties(pageInfo, dishDtoPage,"records");
+
+        List<Dish> records = pageInfo.getRecords();
+        // 将Dish的records转换为DishDto的records,并增加分类名称
+        List<DishDto> list = records.stream().map(dish -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(dish, dishDto);
+            Long categoryId = dish.getCategoryId();
+            if (categoryId != null) {
+                dishDto.setCategoryName(categoryService.getById(categoryId).getName());
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+        dishDtoPage.setRecords(list);
+
+        return Response.success(dishDtoPage);
 
     }
 
