0504c797c2e53caf91b29832d915ac4a5edfc900
MQ <2271443486@qq.com>
2022-04-22 19:57:08 +0800

    分页查询优化

--- src/main/java/com/smallmq/controller/SetMealController.java
+++ src/main/java/com/smallmq/controller/SetMealController.java
@@ -3,12 +3,16 @@ package com.smallmq.controller;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.smallmq.dto.SetmealDto;
 import com.smallmq.pojo.Setmeal;
+import com.smallmq.service.CategoryService;
 import com.smallmq.service.SetMealService;
 import com.smallmq.utils.Response;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/setmeal")
@@ -18,13 +22,30 @@ public class SetMealController {
     @Autowired
     private SetMealService setMealService;
 
+    @Autowired
+    private CategoryService categoryService;
+
+
     // 分页查询数据
     @GetMapping("/page")
     public Response<Page> page(@RequestParam(defaultValue = "1") Integer page,
                                @RequestParam(defaultValue = "5") Integer pageSize) {
         Page<Setmeal> setmealPage = new Page<>(page, pageSize);
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+
         setMealService.page(setmealPage);
-        return Response.success(setmealPage);
+        BeanUtils.copyProperties(setmealPage, setmealDtoPage,"records");
+        setmealDtoPage.setRecords(setmealPage.getRecords().stream().map(setmeal -> {
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(setmeal, setmealDto);
+            Long categoryId = setmeal.getCategoryId();
+            if(categoryId != null) {
+                setmealDto.setCategoryName(categoryService.getById(categoryId).getName());
+                log.info(categoryService.getById(categoryId).getName());
+            }
+            return setmealDto;
+        }).collect(Collectors.toList()));
+        return Response.success(setmealDtoPage);
     }
     // 增加套餐
     @PostMapping
